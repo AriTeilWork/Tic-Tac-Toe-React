dc323e298462c9a7d2b19f396768066ff2b643a2
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-19 15:36:45 +0200

    App.jsx import board and wining-combinations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 
+import { useState } from 'react';
+import './index.css';
+import GameBoard from './components/GameBoard.jsx';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+
+
+
 function App() {
   
 
