b55d02fd6706575ccc454fc13a7011c847fd637b
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-19 15:12:40 +0200

    index.jsx React.createElement()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import { i } from "framer-motion/client";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+//ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
