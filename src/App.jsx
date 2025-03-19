13dfa4ab8286a5efa8c28a6a51727b8fa41b9ed9
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-19 15:41:13 +0200

    App.jsx INITIAL_GAME_BOARD Arrey<Arrey<null>> 3x3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,24 @@ import GameBoard from './components/GameBoard.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
 
+/*
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+};
+*/ //for future use
+
+/**
+ * Represents the initial state of the Tic-Tac-Toe game board.
+ * It is a 3x3 grid where each cell is initialized to `null`.
+ * 
+ * @constant {Array<Array<null>>} INITIAL_GAME_BOARD
+ */
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 
 function App() {
