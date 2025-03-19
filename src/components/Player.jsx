5c414d3146d2525cbe1f7814efcb5d605d3e90ed
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-19 15:50:02 +0200

    index.html

---
+++ src/components/Player.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function Player({ name }) {
+	return (
+		<div>
+			<h2>Player: {name}</h2>
+		</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
-import { i } from "framer-motion/client";
 
 const entryPoint = document.getElementById("root");
 //ReactDOM.createRoot(entryPoint).render(<App />);
 ReactDOM.createRoot(entryPoint).render(React.createElement(App));
+
